602376a2432fed90d8943fe6525a32c8a20849df
PavelOkun <pavelokun17@gmail.com>
2020-03-10 21:29:46 +0300

    feat: add 2 column sort

--- src/components/DataSheet/DataSheet.js
+++ src/components/DataSheet/DataSheet.js
@@ -7,6 +7,8 @@ import {
 } from '@material-ui/core';
 import generateFakeArray from '../../data/fakeDataGenerator';
 
+const _ = require('lodash');
+
 const fakeData = generateFakeArray(1000);
 
 const useStyle = makeStyles({
@@ -80,36 +82,77 @@ const dataToProps = (data) => data.map((item, idx) => {
 
 const DataSheet = () => {
   const classes = useStyle();
-  const [sortType, setSortType] = useState('');
+  const [sortType, setSortType] = useState({
+    currency: '',
+    amount: '',
+  });
   const [fake, setFake] = useState(fakeData);
-  const useSort = () => {
-    const fakeCopy = [...fake];
-    if (sortType === '') {
-      setSortType('asc');
-      fakeCopy.sort((a, b) => a.amount - b.amount);
+  const [clickedField, setClickedField] = useState([]);
+  const useSort = (event, field) => {
+    if (event.shiftKey) {
+      if (clickedField === [] || clickedField === [field]) return; // 1 and 2
+      if (sortType[field] === '') { // 3
+        const sortTypeImm = { ...sortType };
+        sortTypeImm[field] = 'asc';
+        setSortType(sortTypeImm);
+        const fakeCopy = _.orderBy(fake, [clickedField[0], field], [sortType[clickedField[0]], 'asc']);
+        setFake(fakeCopy);
+      } else if (sortType[field] === 'asc') {
+        const sortTypeImm = { ...sortType };
+        sortTypeImm[field] = 'desc';
+        setSortType(sortTypeImm);
+        const fakeCopy = _.orderBy(fake, [clickedField[0], field], [sortType[clickedField[0]], 'desc']);
+        setFake(fakeCopy);
+      } else {
+        const sortTypeImm = { ...sortType };
+        sortTypeImm[field] = '';
+        setSortType(sortTypeImm);
+        const fakeCopy = _.orderBy(fakeData, [clickedField[0]], [sortType[clickedField[0]]]);
+        setFake(fakeCopy);
+      }
+      return;
+    }
+
+    if (sortType[field] === '') {
+      const sortTypeImm = { ...sortType };
+      sortTypeImm[field] = 'asc';
+      setSortType(sortTypeImm);
+      const fakeCopy = _.orderBy(fake, [field], ['asc']);
       setFake(fakeCopy);
-    } else if (sortType === 'asc') {
-      setSortType('desc');
-      fakeCopy.sort((a, b) => b.amount - a.amount);
+    } else if (sortType[field] === 'asc') {
+      const sortTypeImm = { ...sortType };
+      sortTypeImm[field] = 'desc';
+      setSortType(sortTypeImm);
+      const fakeCopy = _.orderBy(fake, [field], ['desc']);
       setFake(fakeCopy);
     } else {
-      setSortType('');
+      const sortTypeImm = { ...sortType };
+      sortTypeImm[field] = '';
+      setSortType(sortTypeImm);
       setFake(fakeData);
     }
+    setClickedField([field]);
   };
+
   return (
     <Table hover bordered>
       <thead>
         <tr>
-          <th className={`${classes.header} ${classes.fixedColHeader}`}>Name</th>
+          <th className={`${classes.header} ${classes.fixedColHeader}`}>
+            Name
+            {/* <button onClick={() => useNameSort(event)}>s</button> */}
+          </th>
           <th className={classes.header}>Gender</th>
           <th className={classes.header}>Date of birth</th>
           <th className={classes.header}>Address</th>
           <th className={classes.header}>Bank</th>
-          <th className={classes.header}>Currency</th>
+          <th className={classes.header}>
+            Currency
+            <button onClick={(field) => useSort(event, field = 'currency')}>s</button>
+          </th>
           <th className={classes.header}>
             Balance
-            <button onClick={() => useSort()}>s</button>
+            <button onClick={(field) => useSort(event, field = 'amount')}>s</button>
           </th>
           <th className={classes.header}>Status</th>
         </tr>
